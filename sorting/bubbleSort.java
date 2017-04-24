4348820b9ee4a6cc5fc225f8296f848e195b95e2
Rklangrin <rklandgren@gmail.com>
2017-04-24 15:12:10 -0500

    completes bubbleSort in Java

---
+++ sorting/bubbleSort.java
@@ -0,0 +1,42 @@
+import java.lang.*;
+import java.util.*;
+
+public class Sort {
+  
+  public void bubbleSort(int[] array) {
+    int length = array.length;
+    
+    for (int k = 0; k < length; k++) {
+      boolean swapped = false;
+      for (int i = 0; i < length - 1; i++) {
+        if (array[i] > array[i + 1]) {
+          int temp = array[i];
+          array[i] = array[i + 1];
+          array[i + 1] = temp;
+          swapped = true;
+        };
+      }
+      if (swapped = false) {
+        break;
+      }
+    }
+  }
+  
+  
+  public void printArray(int[] array) {
+    for (int i = 0; i < array.length; i++) {
+      System.out.print(array[i] + " ");
+    }
+  }
+  
+  public static void main(String[] args) {
+
+    Sort sort = new Sort();
+    int[] array = { 9, 8, 2, 4, 8, 100, 4, 2, 55, 6, 8, 15, 5, 19, 53, 9, 25 };
+    
+    sort.bubbleSort(array);
+    
+    sort.printArray(array);
+    
+  }
+}
