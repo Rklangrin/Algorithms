b43aa62b610792d04a69d3b2094c167a0b50b031
Rklangrin <rklandgren@gmail.com>
2017-04-24 15:21:48 -0500

    changes second occurence of length to array.length

--- sorting/bubbleSort.java
+++ sorting/bubbleSort.java
@@ -7,7 +7,7 @@ public class Sort {
     
     for (int k = 0; k < array.length; k++) {
       boolean swapped = false;
-      for (int i = 0; i < length - 1; i++) {
+      for (int i = 0; i < array.length - 1; i++) {
         if (array[i] > array[i + 1]) {
           int temp = array[i];
           array[i] = array[i + 1];
