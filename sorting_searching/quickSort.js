a0f370b6e7750b5871a3e61ba3bfe3953a98cff0
Rklangrin <rklandgren@gmail.com>
2017-05-03 15:14:29 -0500

    refactors quickSort to use spread operator (es6) and adds comments

--- sorting_searching/quickSort.js
+++ sorting_searching/quickSort.js
@@ -15,10 +15,14 @@ let quickSort = (arr) => {
     }
   }
   
-  let sortedLeft = quickSort(left);
-  let sortedRight = quickSort(right);
-  
-  return sortedLeft.concat(pivot, sortedRight);
+  return [...quickSort(left), pivot, ...quickSort(right)];
+
+  // if someArray = [1, 8, 6] and otherArray = [4, 2, 6]
+  // then [...someArray, ...otherArray] essentially just concatenates the two arrays together
+  // and returns [1, 8, 6, 4, 2, 6]
 } 
 
 console.log(quickSort([1, 2, 4, 9, 3, 7, 5, 3, 4]))
+
+
+// Worst case O(n^2) because if a sorted or nearly sorted array was passed to quickSort, and the pivot was the last number in the array, then it would have to compare every number against every other (or nearly every other if it is nearly sorted) number in the array.
